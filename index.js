ca581eb3c214248d207cc30820849d7d7d5ea62e
martinfs1 <martinfernando-sandander@hotmail.com>
2022-08-05 11:11:45 -0300

    problem with counter of scores and rounds fixed

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 console.log("Hello World!");
 
 function getComputerChoice() {
+
   let randomN = Math.floor(Math.random() * 3);
   const choice = ["rock", "paper", "scissors"];
+
   for (let index = 0; index < choice.length; index++) {
     const element = choice[index];
     if (choice.indexOf(element) === randomN) {
@@ -11,66 +13,62 @@ function getComputerChoice() {
   }
 }
 
-let playerScore=0;
-let computerScore=0;
-let round=0;
-function playRound(
-  playerSelection = () => playerSelection.toLowerCase(),
-  computerSelection
-) {
+let playerScore = 0;
+let computerScore = 0;
+let round = 0;
+
+function playRound(playerSelection = () => playerSelection.toLowerCase(), computerSelection) {
 
   while (playerSelection !== computerSelection) {
-		if(playerSelection === 'rock' && computerSelection === 'scissors'){						
-			playerScore+=1;
-			return playerScore;
-		} else if(playerSelection === 'scissors' && computerSelection === 'rock'){			
-			computerScore+=1;
-			return computerScore;
-		} else if(playerSelection === 'scissors' && computerSelection === 'paper'){			
-			playerScore+=1;
-			return playerScore;
-		} else if(playerSelection === 'paper' && computerSelection === 'scissors'){			
-			computerScore+=1;
-			return computerScore;
-		}  else if(playerSelection === 'paper' && computerSelection === 'rock'){			
-			playerScore+=1;
-			return playerScore;
-		}  else if(playerSelection === 'rock' && computerSelection === 'paper'){			
-			computerScore+=1;
-			return computerScore;
-		} 
-	}
-	return `They're the same. Player with: ${playerScore} points and Computer with: ${computerScore} points `
+
+    if (playerSelection === "rock" && computerSelection === "scissors") {
+      playerScore += 1;
+      return playerScore;
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+      computerScore += 1;
+      return computerScore;
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+      playerScore += 1;
+      return playerScore;
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+      computerScore += 1;
+      return computerScore;
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+      playerScore += 1;
+      return playerScore;
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+      computerScore += 1;
+      return computerScore;
+    }
+  }
+
+  return `They're the same. Player with: ${playerScore} points and Computer with: ${computerScore} points `;
 }
 
 function game() {
 
   for (let index = 0; index < 5; index++) {
+    let playerSelection = prompt("type rock, paper or scissors");
+    if ((playerSelection !== "rock") && (playerSelection !== "scissors") && playerSelection !== "paper") {
+			alert("You ought to put rock, paper or scissors");  			
+    } else {
+			console.log(playerSelection);
+      const computerSelection = getComputerChoice();
+      console.log(computerSelection);
+      playRound(playerSelection, computerSelection);
+      round += 1;
+      console.log(`Round number:${round} Human:${playerScore} and Computer:${computerScore}`);
+    }  
+  }
 
-    const playerSelection = prompt("introduce rock, paper or scissors");
-    console.log(playerSelection);
-    const computerSelection = getComputerChoice();
-    console.log(computerSelection);
-    console.log(playRound(playerSelection, computerSelection));	
-
-		round+=1;		
-		console.log(`Round number: ${round}`)
-
-		if (playerScore === 1 && computerScore === 0) {
-			playerScore +=1;
-		} else if (playerScore === 0 && computerScore === 1) {
-			computerScore +=1;
-		}
-	}
-	
 	if (playerScore > computerScore) {
-		return `The winner is Player with:${playerScore} points and Computer with:${computerScore}`
+		return `The winner is Player with:${playerScore} points and Computer with:${computerScore}`;
+	} else if (computerScore > playerScore) {
+		return `The winner is Computer:${computerScore} points and Player:${playerScore} points`;
 	} else {
-		return `The winner is Computer with:${computerScore} points and Player with:${playerScore} points`		
+		return `There is a tie Human:${playerScore} - Computer:${computerScore}`;
 	}
-
 }
 
-// debug playerScore in second round add two times.
-
 console.log(game());
+// fix problem with prove the correct string put by the player
